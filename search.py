56efa2207dd0dbc969cf575a40ebd388f3784769
Rick Henry <fredericmhenry@gmail.com>
2019-05-26 14:02:40 -0400

    use native airtable formulas, remove fuzzywuzzy
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- search.py
+++ search.py
@@ -1,7 +1,6 @@
 import json
 
 from common import setup_airtable, validate_request, make_filter_formula
-from fuzzywuzzy import fuzz
 
 from responses import failure, success
 
@@ -32,37 +31,15 @@ def assigned(event, _context):
     if err_response:
         return err_response
     try:
-        result = search_helper(
-            "Assigned To", data["assignedTo"], multiple=True, exact=False
+        at = setup_airtable()
+        results = at.get_all(
+            formula=(
+                "SEARCH('" + data["assignedTo"].lower() + "', LOWER({Assigned To}))"
+            )
         )
-        if result:
-            return success(result)
+        if results:
+            return success(results)
         else:
             return failure(f"No matching instrument was found", 404)
     except Exception as err:
         return failure(f"Something has gone wrong {err}")
-
-
-def search_helper(field, value, multiple=False, exact=True):
-    """Helper to do the necessary search logic"""
-    at = setup_airtable()
-    results = []
-    for page in at.get_iter():
-        for item in page:
-            if _check_item(item, field, value, exact):
-                if not multiple:
-                    return item
-                results.append(item)
-
-    return results
-
-
-def _check_item(item, field, value, exact):
-    if field not in item["fields"]:
-        return False
-    actual_value = item["fields"].get(field)
-    if actual_value.lower() == value.lower():
-        return True
-    elif not exact and fuzz.partial_ratio(value.lower(), actual_value.lower()) > 80:
-        return True
-    return False
--- tests/test_search.py
+++ tests/test_search.py
@@ -5,73 +5,6 @@ from unittest import mock
 import search
 
 
-# def test_search_helper_single(monkeypatch, fake_airtable, records):
-#     """Test the search helper function gets one result"""
-#     monkeypatch.setattr("search.setup_airtable", lambda: fake_airtable)
-#
-#     results = search.search_helper("Number", "1-605")
-#
-#     assert results == records[0]
-
-
-def test_search_helper_multiple(monkeypatch, fake_airtable, records):
-    """Test the search helper function gets all results"""
-    monkeypatch.setattr("search.setup_airtable", lambda: fake_airtable)
-
-    results = search.search_helper("Assigned To", "Test Name", multiple=True)
-
-    assert {rec["id"] for rec in results} == {records[1]["id"], records[8]["id"]}
-
-
-def test_search_helper_single_gets_only_one(monkeypatch, fake_airtable):
-    """
-    Test that the search helper gets only one result even when there are multiple
-    matches.
-    """
-    monkeypatch.setattr("search.setup_airtable", lambda: fake_airtable)
-
-    result = search.search_helper("Assigned To", "Test Name", multiple=False)
-
-    assert not isinstance(result, list)
-
-
-def test_search_helper_partial_matches(monkeypatch, fake_airtable, records):
-    """Test that the search helper returns partial patches"""
-    monkeypatch.setattr("search.setup_airtable", lambda: fake_airtable)
-
-    results = search.search_helper("Assigned To", "Name", multiple=True, exact=False)
-
-    assert len(results) == 4
-    assert {rec["id"] for rec in results} == {
-        records[0]["id"],
-        records[1]["id"],
-        records[2]["id"],
-        records[8]["id"],
-    }
-
-
-def test_search_number(monkeypatch, search_number_event):
-    """Test searching for an instrument number"""
-    found = {
-        "id": "rec15",
-        "fields": {
-            "Number": "1-201",
-            "Instrument Type": "violin",
-            "Assigned To": "Test Name",
-        },
-    }
-    at_mock = mock.MagicMock()
-    at_mock.get_all.return_value = found
-    monkeypatch.setattr("search.setup_airtable", lambda: at_mock)
-
-    response = search.number(search_number_event, {})
-
-    at_mock.get_all.assert_called_with(formula="{Number}='1-201'")
-
-    assert response["statusCode"] == 200
-    assert response["body"] == json.dumps(found)
-
-
 def test_search_number_not_found(monkeypatch, search_number_event):
     """Test searching for an instrument number"""
     found = []
@@ -96,14 +29,14 @@ def test_search_number_bad_request():
 def test_search_assigned(monkeypatch, search_assigned_event, records):
     """Test searching for a student name"""
     found = [records[1], records[2], records[8]]
-    search_helper_mock = mock.MagicMock()
-    search_helper_mock.return_value = found
-    monkeypatch.setattr("search.search_helper", search_helper_mock)
+    at_mock = mock.MagicMock()
+    at_mock.get_all.return_value = found
+    monkeypatch.setattr("search.setup_airtable", lambda: at_mock)
 
     response = search.assigned(search_assigned_event, {})
 
-    search_helper_mock.assert_called_with(
-        "Assigned To", "Test Name", multiple=True, exact=False
+    at_mock.get_all.assert_called_with(
+        formula="SEARCH('test name', LOWER({Assigned To}))"
     )
     assert response["statusCode"] == 200
     assert response["body"] == json.dumps(found)
